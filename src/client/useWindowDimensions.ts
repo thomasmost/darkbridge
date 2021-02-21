aa4e89285914e801caec6280e0d967e3c540cc32
tcm_NIMBUS <tomconmo@gmail.com>
2021-02-21 18:23:43 -0500

    :beetle: fix SSR breaking on window object

--- src/client/useWindowDimensions.ts
+++ src/client/useWindowDimensions.ts
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
+  if (typeof window === 'undefined') {
+    return {
+      width: 1000,
+      height: 800,
+    };
+  }
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
@@ -19,8 +25,14 @@ export function useWindowDimensions() {
       setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize);
+    }
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize);
+      }
+    };
   }, []);
 
   return windowDimensions;
