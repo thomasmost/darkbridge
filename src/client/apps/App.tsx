d3bf1423b6a7ffc9ff7bf62945d4baa79fe1dbb6
tcm_NIMBUS <tomconmo@gmail.com>
2021-02-22 14:34:09 -0500

    :beetle: REVERT: "try eliminating the fixed footer to see if that is causing the inputs to lose focus"
    This reverts commit 1931bdfd343a35a82a06b2bf4de2ae2536c28c33.

--- src/client/apps/App.tsx
+++ src/client/apps/App.tsx
@@ -92,6 +92,7 @@ const App = () => {
                 <Logout path="logout" />
               </Router>
             </Main>
+            {shouldRenderPWA && <FooterPWA />}
           </AppContainer>
         </MuiPickersUtilsProvider>
       </ThemeProvider>
