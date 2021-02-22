1931bdfd343a35a82a06b2bf4de2ae2536c28c33
tcm_NIMBUS <tomconmo@gmail.com>
2021-02-22 12:35:06 -0500

    :beetle: try eliminating the fixed footer to see if that is causing the inputs to lose focus

--- src/client/apps/App.tsx
+++ src/client/apps/App.tsx
@@ -92,7 +92,6 @@ const App = () => {
                 <Logout path="logout" />
               </Router>
             </Main>
-            {shouldRenderPWA && <FooterPWA />}
           </AppContainer>
         </MuiPickersUtilsProvider>
       </ThemeProvider>
