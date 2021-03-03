d11db359b505ac04143be0e963b0b5b0753890bc
tcm_NIMBUS <tomconmo@gmail.com>
2021-03-03 10:44:05 -0500

    :test_tube: added seedData script and a static list of timezones

---
+++ dev/seedData.ts
@@ -0,0 +1,182 @@
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import dotenv from 'dotenv';
+import faker from 'faker';
+
+dotenv.config();
+import { User } from '../src/models/user.model';
+import {
+  ClientProfile,
+  ClientProfileCreationAttributes,
+} from '../src/models/client_profile.model';
+import { timezones_by_utc_string } from '../src/data/timezones';
+import { DateTimeHelper } from '../src/helpers/datetime.helper';
+import { createAppointmentForClient } from '../src/helpers/appointment.helper';
+import { format } from 'date-fns';
+import { Appointment } from '../src/models/appointment.model';
+
+const CLIENTS_PER_USER = 10;
+const DAYS_OF_APPOINTMENTS = 7;
+
+const commonTimes = [
+  '09:00:00',
+  '10:30:00',
+  '12:00:00',
+  '13:30:00',
+  '15:00:00',
+  '16:30:00',
+  '18:00:00',
+];
+const summaries = [
+  'fix door',
+  'leaky faucet',
+  'leaky drainpipe',
+  'flooded basement',
+  'sparking wire',
+  'flashing lights',
+  'broken fan',
+  'broken AC',
+  'no heat',
+];
+
+async function createUsers() {
+  const users = [
+    {
+      email: 'tomismore@gmail.com',
+      password: 'thomas',
+      given_name: 'Thomas',
+      family_name: 'Moore',
+    },
+    {
+      email: 'electrical@callteddy.com',
+      password: 'testing',
+    },
+    {
+      email: 'plumbing@callteddy.com',
+      password: 'testing',
+    },
+    {
+      email: 'hvac@callteddy.com',
+      password: 'testing',
+    },
+    {
+      email: 'carpentry@callteddy.com',
+      password: 'testing',
+    },
+  ];
+
+  const promises = [];
+  for (const user of users) {
+    const { email, password, family_name, given_name } = user;
+    const password_salt = crypto.randomBytes(16).toString();
+
+    const seasoned_password = `${password_salt}:${password}`;
+
+    const password_hash = await bcrypt.hash(seasoned_password, 12);
+
+    promises.push(
+      User.create({
+        email,
+        password_hash,
+        password_salt,
+        family_name: family_name || faker.name.lastName(),
+        given_name: given_name || faker.name.firstName(),
+      }),
+    );
+  }
+  return Promise.all(promises);
+}
+
+async function createClients(created_by_user_id: string) {
+  const promises = [];
+  for (let i = 0; i < CLIENTS_PER_USER; i++) {
+    const timezone = faker.address.timeZone();
+    const static_timezone = timezones_by_utc_string[timezone];
+    if (!static_timezone) {
+      console.log('Could not find static timezone for ', timezone);
+    }
+    const timezone_offset = static_timezone?.offset || 0;
+    const email = faker.internet.email();
+    const full_name = faker.name.findName();
+    const phone = faker.phone.phoneNumber();
+    const client_data: ClientProfileCreationAttributes = {
+      created_by_user_id,
+      email,
+      full_name,
+      phone,
+      timezone,
+      timezone_offset,
+      address_street: faker.address.streetAddress(),
+      address_city: faker.address.city(),
+      address_state: faker.address.state(),
+      address_postal_code: faker.address.zipCode(),
+    };
+
+    promises.push(ClientProfile.create(client_data));
+  }
+  return Promise.all(promises);
+}
+
+async function createAppointmentsForUser(
+  user_id: string,
+  clients: ClientProfile[],
+) {
+  const today = new Date();
+  const promises = [];
+  for (let i = 0; i < DAYS_OF_APPOINTMENTS; i++) {
+    const dateForAppointments = DateTimeHelper.add(today, i, 'days');
+    const datePartial = format(dateForAppointments, 'yyyy-MM-dd');
+    const promise = createAppointmentsForUserDay(user_id, clients, datePartial);
+    promises.push(promise);
+  }
+  return Promise.all(promises);
+}
+
+async function createAppointmentsForUserDay(
+  user_id: string,
+  clients: ClientProfile[],
+  date: string,
+) {
+  const promises = [];
+  const appointmentsForDay = Math.floor(Math.random() * 4);
+  for (let i = 0; i < appointmentsForDay; i++) {
+    const randomTime =
+      commonTimes[Math.floor(Math.random() * commonTimes.length)];
+    const randomClient = clients[Math.floor(Math.random() * clients.length)];
+    const datetimeLocal = date + ' ' + randomTime;
+    const randomSummary =
+      summaries[Math.floor(Math.random() * summaries.length)];
+    const randomPriority = 'P2';
+    const randomDuration = 60;
+    const promise = createAppointmentForClient(
+      user_id,
+      randomClient.id,
+      datetimeLocal,
+      randomDuration,
+      randomPriority,
+      randomSummary,
+    ).catch((err) => {
+      console.log('Creating an appointment failed due to:');
+      console.log(err);
+    });
+    promises.push(promise);
+  }
+  return Promise.all(promises);
+}
+
+async function truncate() {
+  await User.destroy({ truncate: true });
+  await ClientProfile.destroy({ truncate: true });
+  await Appointment.destroy({ truncate: true });
+}
+
+async function main() {
+  await truncate();
+  const users = await createUsers();
+  for (const user of users) {
+    const clients = await createClients(user.id);
+    await createAppointmentsForUser(user.id, clients);
+  }
+}
+
+main();
