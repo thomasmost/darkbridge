a63d5e7f85be6aa230a55e86f98cad3c42042621
tcm_NIMBUS <tomconmo@gmail.com>
2021-03-23 01:37:03 -0400

    :handshake: make state a dropdown on client profile creation

--- src/client/pages/AddClientProfile.tsx
+++ src/client/pages/AddClientProfile.tsx
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled';
 import { RouteComponentProps, useNavigate } from '@reach/router';
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useForm } from 'react-hook-form';
+import { Styles } from 'react-select';
 import { toast } from 'react-toastify';
+import Select from 'react-select';
+
+import { isoStates } from '../../data/iso_states';
 import {
   ClientProfileAttributes,
   ClientProfileCreationAttributes,
@@ -20,21 +24,58 @@ const Label = styled.label`
   margin-bottom: 10px;
 `;
 
-type ClientProfileFormValues = Pick<
+type ClientProfileFormValues = Omit<
   ClientProfileCreationAttributes,
   'created_by_user_id'
 >;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const selectStyles: Styles<any, false> = {
+  container: (provided) => ({
+    ...provided,
+    cursor: 'pointer',
+    width: '100%',
+  }),
+  control: (provided, props) => ({
+    ...provided,
+    border: 'none',
+    borderRadius: '10px',
+    cursor: 'pointer',
+    padding: '5px 10px',
+    marginBottom: '20px',
+    backgroundColor: theme.inputBackgroundColor,
+    boxShadow: props.isFocused ? '4px 4px #23C38A' : 'none',
+  }),
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    display: 'none',
+  }),
+  option: (provided, option) => ({
+    ...provided,
+    cursor: 'pointer',
+    fontStyle: option.label === 'Add a New Client' ? 'italic' : 'normal',
+  }),
+};
+
 export const AddClientProfile: React.FC<RouteComponentProps> = () => {
+  const states = useMemo(
+    () => isoStates().filter((state) => state.subdivision_category === 'state'),
+    [],
+  );
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm<ClientProfileFormValues>();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+  } = useForm<ClientProfileFormValues>();
   const { user } = useAuth();
+  useEffect(() => {
+    register('address_state');
+  }, [register]);
   if (!user) {
     return null;
   }
-
   const onSubmit = async (data: ClientProfileFormValues) => {
-    console.log(data);
     const result = await apiRequest<ClientProfileAttributes>(
       'client_profile',
       'json',
@@ -62,27 +103,23 @@ export const AddClientProfile: React.FC<RouteComponentProps> = () => {
         />
         <div>
           <Label>Street Address</Label>
-          <Input
-            name="address_street"
-            // defaultValue={profile.email}
-            ref={register()}
-          />
+          <Input name="address_street" ref={register()} />
         </div>
         <FlexColumns>
           <div>
             <Label>City</Label>
-            <Input
-              name="address_city"
-              // defaultValue={profile.email}
-              ref={register()}
-            />
+            <Input name="address_city" ref={register()} />
           </div>
-          <div>
+          <div style={{ width: '25%' }}>
             <Label>State</Label>
-            <Input
-              name="address_state"
-              // defaultValue={profile.email}
-              ref={register()}
+            <Select
+              getOptionLabel={(item) => item.name}
+              getOptionValue={(item) => item.name}
+              options={states}
+              styles={selectStyles}
+              onChange={(selection) =>
+                setValue('address_state', selection.name)
+              }
             />
           </div>
           <div>
