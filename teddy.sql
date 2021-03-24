241c77e8f68a3b680c8ebe0f4b8665aa593fd77b
tcm_NIMBUS <tomconmo@gmail.com>
2021-03-24 17:12:47 -0400

    :pancakes: drop full_name from client_profile table

--- teddy.sql
+++ teddy.sql
@@ -131,7 +131,6 @@ create table `client_profile` (
   user_id VARCHAR(255) NULL,
   created_by_user_id VARCHAR(255) NOT NULL,
   email VARCHAR(255) NULL,
-  full_name VARCHAR(255) NULL,
   given_name VARCHAR(255) NULL,
   family_name VARCHAR(255) NULL,
   phone VARCHAR(255) NULL,
