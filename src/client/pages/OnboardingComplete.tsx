0584d7a52c5544f3f0dbaaccb96a8a90344250cd
Thomas Constantine Moore <thomas@callteddy.com>
2021-05-20 15:34:57 -0400

    :rocket: set onboarding completed to true on last slide of onboarding for web, closes #52

--- src/client/pages/OnboardingComplete.tsx
+++ src/client/pages/OnboardingComplete.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import styled from '@emotion/styled';
 import { H3, Logo, OnboardingNav } from '../elements/OnboardingElements';
+import { putRequest } from '../services/api.svc';
 
 const FinalH3 = styled(H3)`
   text-align: center;
@@ -18,11 +19,18 @@ const Button = styled.button`
 `;
 
 export const OnboardingComplete: React.FC<RouteComponentProps> = () => {
+  const onSubmit = async () => {
+    await putRequest('contractor_profile', 'text', {
+      onboarding_completed: true,
+    });
+    location.assign('/');
+  };
+
   return (
     <div>
       <Logo height="64px" src="/logo_light.svg" />
       <FinalH3>You&apos;re all set!</FinalH3>
-      <Button onClick={() => location.assign('/')}>Go to my Dashboard</Button>
+      <Button onClick={() => onSubmit()}>Go to my Dashboard</Button>
       <OnboardingNav slideNumber={5} hideContinueButton />
     </div>
   );
